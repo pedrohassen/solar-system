08bd14fe01366cf97e9850583737bfb87f9a1dc1
Pedro <pedro_hasse@yahoo.com.br>
2022-07-20 18:46:39 -0300

    :sparkles: Requisito 7 ok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header1 from './components/Header';
+import Missions1 from './components/Missions';
 import SolarSystem1 from './components/SolarSystem';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
       <div>
         <Header1 />
         <SolarSystem1 />
+        <Missions1 />
       </div>
     );
   }
---
+++ src/components/Missions.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Missions extends Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        Missions
+      </div>
+    );
+  }
+}
+
+export default Missions;
