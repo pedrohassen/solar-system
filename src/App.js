cf988dbffa157b71ab9d0aba506d4d4f1086534f
Pedro <pedro_hasse@yahoo.com.br>
2022-07-20 15:03:49 -0300

    :sparkles: Requisito 2 ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Title from './components/Header';
+import Solar from './components/SolarSystem';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Title />
+        <Solar />
       </div>
     );
   }
---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        SolarSystem
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
